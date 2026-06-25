af29b4a5194f76fcefe60f09b87d25434350fbd5
renunegii <officialrenu1@gmail.com>
2026-06-25 15:00:23 +0530

    updated mysql workbench queries

--- Data Cleaning- SQL Queries.sql
+++ Data Cleaning- SQL Queries.sql
@@ -1,1 +1,13 @@
-select * from customer_data;
+SET SQL_SAFE_UPDATES = 0;
+-- Should show 6418
+SELECT COUNT(*) FROM customer_data;
+
+-- See first 3 rows
+SELECT * FROM customer_data LIMIT 3;
+
+-- Checking for Empty values
+SELECT COUNT(*) AS real_nulls 
+FROM customer_data 
+WHERE Value_Deal = '';
+
+-- 
